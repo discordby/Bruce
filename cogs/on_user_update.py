1691a317e7c8025b567310609d27a198c0819dba
discordby <86150771+discordby@users.noreply.github.com>
2021-07-19 16:33:19 +0300

    Update on_user_update.py

--- cogs/on_user_update.py
+++ cogs/on_user_update.py
@@ -5,8 +5,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 ## Firebase Database ##
-firebase_config = {"apiKey": "AIzaSyDe_xKKup4lVoPasLmAQW9Csc1zUzsxB0U","authDomain": "chuckwalla-69.firebaseapp.com",
-  "databaseURL": "https://chuckwalla-69.firebaseio.com","storageBucket": "chuckwalla-69.appspot.com",
+firebase_config = {"apiKey": "AIzaSyAr88_37tciNauGiRs73B_PrKGydwG_d1U","authDomain": "alert-result-315817.firebaseapp.com",
+  "databaseURL": "https://alert-result-315817-default-rtdb.europe-west1.firebasedatabase.app","storageBucket": "alert-result-315817.appspot.com",
   "serviceAccount": json.loads(os.getenv("serviceAccountKeyJSON"))}
 db = pyrebase.initialize_app(firebase_config).database()
 
