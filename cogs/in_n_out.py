d4e8c26c1600a58d334bb15903deae3abaad0dce
discordby <86150771+discordby@users.noreply.github.com>
2021-07-20 01:23:00 +0300

    Update in_n_out.py

--- cogs/in_n_out.py
+++ cogs/in_n_out.py
@@ -20,8 +20,8 @@ yarrow_emoji = config.get('yarrow_emoji')
 rarrow_emoji = config.get('rarrow_emoji')
 
 ## Firebase Database ##
-firebase_config = {"apiKey": "AIzaSyDe_xKKup4lVoPasLmAQW9Csc1zUzsxB0U","authDomain": "chuckwalla-69.firebaseapp.com",
-  "databaseURL": "https://chuckwalla-69.firebaseio.com","storageBucket": "chuckwalla-69.appspot.com",
+firebase_config = {"apiKey": "AIzaSyAr88_37tciNauGiRs73B_PrKGydwG_d1U","authDomain": "alert-result-315817.firebaseapp.com",
+  "databaseURL": "https://alert-result-315817-default-rtdb.europe-west1.firebasedatabase.app","storageBucket": "alert-result-315817.appspot.com",
   "serviceAccount": json.loads(os.getenv("serviceAccountKeyJSON"))}
 db = pyrebase.initialize_app(firebase_config).database()
 
